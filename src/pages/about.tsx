f8ee380dfbd192fccf6eac78a99125403322d90a
= <=>
2023-05-03 09:33:01 +0530

    modified styles

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      >
+        <h1 className='text-xl font-bold font-sans'>About Page</h1>
+      </main>
+    )
+  }
